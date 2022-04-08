f8f771bf106a598b063cf0a9b85c6c42f7b2fa8c
salsabila <16520027@std.stei.itb.ac.id>
2022-04-08 22:54:11 +0700

    monster dan player

--- Monster.java
+++ Monster.java
@@ -4,48 +4,77 @@ public class Monster {
     private List<ElementType> elementTypes = new ArrayList<ElementType>();
     private Stats baseStats;
     private List<Move> moves = new ArrayList<Move>();
-  
-    public Monster(){
+    int selectedMove = -1;
+
+    public Monster() {
         baseStats = new Stats();
     }
 
-    public Monster(int id, String name, List<ElementType> elementTypes, double healthPoint, double attack, double defense, double specialAttack, double specialDefense, double speed, List<Move> moves){
+    public Monster(int id, String name, List<ElementType> elementTypes, double healthPoint, double attack,
+            double defense, double specialAttack, double specialDefense, double speed, List<Move> moves) {
         this.id = id;
         this.name = name;
         this.elementTypes = elementTypes;
         this.baseStats = new Stats(healthPoint, attack, defense, specialAttack, specialDefense, speed);
         this.moves = moves;
     }
-    public Monster(int id, String name,double healthPoint, double attack, double defense, double specialAttack, double specialDefense, double speed){
+
+    public Monster(int id, String name, double healthPoint, double attack, double defense, double specialAttack,
+            double specialDefense, double speed) {
         this.id = id;
         this.name = name;
         this.baseStats = new Stats(healthPoint, attack, defense, specialAttack, specialDefense, speed);
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public List<ElementType> getElementType (){
+
+    public List<ElementType> getElementType() {
         return elementTypes;
     }
-    public void addElementType(ElementType el){
+
+    public void addElementType(ElementType el) {
         elementTypes.add(el);
     }
 
-    public Stats getStats(){
+    public Stats getStats() {
         return baseStats;
     }
-    public List<Move> getMoves(){
+
+    public List<Move> getMoves() {
         return moves;
     }
-    public void addMoves (Move move){
+
+    public void addMoves(Move move) {
         moves.add(move);
     }
 
-}
-    
+    public void setMove(int selectedMove) {
+        this.selectedMove = selectedMove;
+    }
 
+    public Move getMove() {
+        try {
+            return this.currentMonster.setMove(selected - 1);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    /*
+     * Move use = currentMonster.getMove();
+     * if (user != null) {
+     * if use.getMoveType().equals("DEFAULT"){
+     * use
+     * }
+     * }
+     */
+
+    public void ChangeMonster() {
+        this.selectedMove = -1;
+    }
 }
--- Player.java
+++ Player.java
@@ -5,11 +5,15 @@ public class Player {
     private int id;
     private String name;
     private List<Monster> monsterList = new ArrayList<Monster>();
+    private Monster currentMonster;
+
+    Scanner scan = new Scanner(System.in);
 
     public Player(int id, String name, List<Monster> monsterList) {
         this.id = id;
         this.name = name;
         this.monsterList = monsterList;
+        this.currentMonster = monsterList.get(0);
     }
 
     public Player(String name) {
@@ -26,6 +30,37 @@ public class Player {
 
     public List<Monster> getMonsters() {
         return this.monsterList;
+    }
+
+    // buat switch monster
+    public void switchMonster() {
+        this.printMonsters();
+        System.out.println("input id monster:");
+        try {
+            int selected = scan.nextInt();
+            Monster temporaryMonster = this.monsterList.get(selected - 1);
+            if (temporaryMonster.getStats().getHealthPoint() <= 0) {
+                System.out.println("monster sudah mati");
+                this.switchMonster();
+            } else {
+                System.out.println("monster sudah diganti");
+                this.currentMonster.ChangeMonster();
+                this.currentMonster = temporaryMonster;
+            }
+        } catch (Exception e) {
+            System.out.println("monster is unavailable");
+            this.switchMonster();
+        }
+
+    }
+
+    public void selectMove(){
+        System.out.println("select move:");
+        int selected = scan.nextInt();
+       // if (this.currentMonster.)
+       else {
+            this.currentMonster.setMove(selected -1);
+       }
     }
 
     public void printMonsters() {
